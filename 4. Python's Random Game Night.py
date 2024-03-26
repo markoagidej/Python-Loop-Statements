c45abe70d27319ae217c9cdc6f7ca09149a8b691
Marko <markogidej@yahoo.com>
2024-03-26 18:21:10 -0400

    Answered Question 4 all tasks

--- 4. Python's Random Game Night.py
+++ 4. Python's Random Game Night.py
@@ -1,2 +1,14 @@
 # Task 1: Random Choice Game
 
+import random
+
+numbers = [1, 2, 3, 4, 5]
+
+chosen = random.choice(numbers)
+
+guess = int(input("Guess a number from 1-5"))
+
+if chosen == guess:
+    print("The right number was" + " " + str(chosen) + ". Lucky Guess...")
+else:
+    print("The right number was" + " " + str(chosen) + ". Try looking at Unix time and calculating the answer!")
