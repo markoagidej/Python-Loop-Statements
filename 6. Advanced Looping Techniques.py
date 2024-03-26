fb8816f8c12557f87e8d15dadaa42898ca11c616
Marko <markogidej@yahoo.com>
2024-03-26 19:46:41 -0400

    Answered Question 6 all tasks

--- 6. Advanced Looping Techniques.py
+++ 6. Advanced Looping Techniques.py
@@ -1,12 +1,18 @@
 # Task 1: The Selective DJ
 
-
+genres = ['Jazz', 'Rock', 'Hip-hop', 'Classical']
+genres_sublist = genres[1:]
 
 
 # Task 2: The One-Liner Band with List Comprehensions
 
-
+music_list = [str(genre) + " Music" for genre in genres]
+print(music_list)
 
 
 # Task 3: Numerical Beats with range
 
+for countdown in range(10, 0, -1):
+    print(countdown)
+
+print("The beat drops now!")
