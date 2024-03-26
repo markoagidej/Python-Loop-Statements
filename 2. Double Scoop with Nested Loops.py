0529ac2b1f19695cfe3798914fedee8c5b864b70
Marko <markogidej@yahoo.com>
2024-03-26 18:01:39 -0400

    Answered Question 2 all tasks

--- 2. Double Scoop with Nested Loops.py
+++ 2. Double Scoop with Nested Loops.py
@@ -1,2 +1,11 @@
 # Task 1: Your Mood Tracker
 
+import random
+
+moods = ["Happy", "Sad", "Energetic", "Calm"]
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+times = ["morning", "afternoon", "evening"]
+
+for day in days:
+    for time in times:
+        print(f"On" + " " + day + " " + time + " " + "you felt" + " " + random.choice(moods))
